34a7fdb95856dd5bd61baa57d86789eceeb65db1
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 17:45:39 -0500

    async function for get movies madde

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,9 @@
 import { checkError, client } from './client';
+
+export async function getMovies(){
+  const response = await client
+    .from('movies')
+    .select();
+
+  return checkError(response);
+}
