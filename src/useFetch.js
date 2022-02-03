8fd5f4938dc092c1d5ff6c9c5cb1222f37141d36
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 18:49:06 -0500

    set state

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,5 +2,7 @@ import { useState, useEffect } from 'react';
 import { getMovies } from './services/fetch-utils';
 
 export default function useFetch() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
   return <div></div>;
 }
