f7a423322dc2bde7901a06fa8639de09219a6ceb
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 18:42:45 -0500

    loading state set

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
   return (
     <div className="App">
      
