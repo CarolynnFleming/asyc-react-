a34cc4f0e2d1eb3e925b345ff7696db85ad84a00
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 17:58:15 -0500

    set state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { getMovies } from './services/fetch-utils.js';
+import { useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
   return (
     <div className="App">
      
