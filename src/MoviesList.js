78a3b5706e8763c7109f7c74a89be7a455ea1b84
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 18:39:24 -0500

    mapped through table items

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function MoviesList({ movies }) {
-  return <div></div>;
+  return <div>
+    {movies.map(({ name, is_classic, famous_directors, famous_actor }, i) => <div key={`${name}-${i}`}>
+      <p>{name}</p>
+      <p>{is_classic ? 'forever in our hearts' : 'generic'}</p>
+      <ul>{famous_directors}</ul>
+      <ul>{famous_actor}</ul>
+    </div>)
+    }
+  </div>;
 }
