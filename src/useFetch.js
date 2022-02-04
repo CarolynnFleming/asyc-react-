a5404d99736ac3da5df19478ad4ff54399223593
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 21:11:30 -0500

    added state and import

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
-import { getMovies } from './services/fetch-utils';
+import { getMovies, getMusic } from './services/fetch-utils';
+
 
 export default function useFetch() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [music, setMusic] = useState([]);
+  const [musicLoading, setMusicLoading] = useState(false);
 
   async function fetchData() {
     setLoading(true);
