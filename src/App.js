7e7d76c1831c12b4768e2da601a5b5bcba339084
CarolynnFleming <cfcatpink@gmail.com>
2022-02-03 20:18:15 -0500

    spinner added classname added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { getMovies } from './services/fetch-utils';
 import MoviesList from './MoviesList';
 import { useState, useEffect } from 'react';
+import homer from './spinner.gif';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,8 +14,22 @@ function App() {
     setLoading(false);
     setMovies(data);
   }
+
+  useEffect(() =>{
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
+      <button onClick={fetchData}>Refetch</button>
+      <h1>Movies</h1>
+      {
+        isLoading
+          ? <img src={homer}/>
+
+          : <MoviesList movies={movies}/>
+      }
      
     </div>
   );
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function MoviesList({ movies }) {
-  return <div>
+  return <div className='movie-list'>
     {movies.map(({ name, is_classic, famous_directors, famous_actor }, i) => <div key={`${name}-${i}`}>
-      <p>{name}</p>
+      <h3>{name}</h3>
       <p>{is_classic ? 'forever in our hearts' : 'generic'}</p>
       <ul>{famous_directors}</ul>
       <ul>{famous_actor}</ul>
