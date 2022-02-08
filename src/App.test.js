7a7b48838afca56dc04b8eaabc83a2ffcbeca01f
CarolynnFleming <cfcatpink@gmail.com>
2022-02-07 19:00:42 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = await screen.findByText(/Music/i);
+  const linkElement = screen.getByText(/Music/i);
   expect(linkElement).toBeInTheDocument();
 });
